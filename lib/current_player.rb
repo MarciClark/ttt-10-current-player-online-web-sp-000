30c189763a5faed8cf966563c9a17bd8d7bc709b
Marci Clark <marcig612@gmail.com>
2019-07-28 17:45:57 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,11 @@
 def turn_count(board)
-  counter = 0
-  if turn_count
-    counter += 1
+  turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
+    end
   end
+  return turns
 end
 
 def current_player
@@ -13,16 +16,6 @@ def current_player
   end
 end
 
-
-def turn_count(board)
-  turns = 0
-  board.each do |space|
-    if space == "X" || space == "O"
-      turns += 1
-    end
-  end
-  return turns
-end
 
 def current_player(board)
   turns = turn_count(board)
